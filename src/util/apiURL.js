17954bd776f2b23830fd5e293a7cb30cbdf52b73
Coreen Cooper <coreencooper@pursuit.org>
2021-06-20 16:45:42 -0400

    update Heroku url

--- src/util/apiURL.js
+++ src/util/apiURL.js
@@ -1,7 +1,6 @@
 export const apiURL = () => {
   if (process.env.NODE_ENV === "development") {
-    return "http://localhost:3001";
-  } else if (process.env.NODE_ENV === "production") {
     return "https://peaceful-cliffs-68239.herokuapp.com/";
   }
+  return "";
 };
